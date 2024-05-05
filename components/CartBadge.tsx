c010db54ac77cacdda651dfb8133d42d90ccb5b6
pensjonatus <pensjonatus@gmail.com>
2024-05-05 06:00:09 +0000

    fix badge display

--- components/CartBadge.tsx
+++ components/CartBadge.tsx
@@ -2,73 +2,64 @@ import jeff from "@/public/jeff.png";
 import Image from "next/image";
 
 export default function CartBadge({ itemCount }: { itemCount: number }) {
-  if (itemCount === 69) {
-    return <>❤️</>;
-  }
+  switch (true) {
+    case itemCount === 69:
+      return <>❤️</>;
 
-  if (itemCount >= 100) {
-    return <>💰</>;
-  }
+    case itemCount === 420:
+      return <>🌿</>;
 
-  if (itemCount === 420) {
-    return <>🌿</>;
-  }
+    case itemCount === 666:
+      return <>😈</>;
 
-  if (itemCount >= 500) {
-    return <>🤑</>;
-  }
+    case itemCount >= 100:
+      return <>💰</>;
 
-  if (itemCount >= 1000) {
-    return <>👑</>;
-  }
+    case itemCount >= 500:
+      return <>🤑</>;
 
-  if (itemCount >= 5000) {
-    return <>💎</>;
-  }
+    case itemCount >= 1000:
+      return <>👑</>;
 
-  if (itemCount >= 10000) {
-    return <>🥇</>;
-  }
+    case itemCount >= 5000:
+      return <>💎</>;
 
-  if (itemCount >= 50000) {
-    return <>🏆</>;
-  }
+    case itemCount >= 10000:
+      return <>🥇</>;
 
-  if (itemCount >= 100000) {
-    return <>🤩</>;
-  }
+    case itemCount >= 50000:
+      return <>🏆</>;
 
-  if (itemCount >= 500000) {
-    return <>🦁</>;
-  }
+    case itemCount >= 100000:
+      return <>🤩</>;
 
-  if (itemCount >= 1000000) {
-    return <>🙌</>;
-  }
+    case itemCount >= 500000:
+      return <>🦁</>;
 
-  if (itemCount >= 5000000) {
-    return <>🤑🤑</>;
-  }
+    case itemCount >= 1000000:
+      return <>🙌</>;
 
-  if (itemCount >= 10000000) {
-    return <>🤑🤑🤑</>;
-  }
+    case itemCount >= 5000000:
+      return <>🤑🤑</>;
 
-  if (itemCount >= 50000000) {
-    return <>🧐</>;
-  }
+    case itemCount >= 10000000:
+      return <>🤑🤑🤑</>;
 
-  if (itemCount >= 100000000) {
-    return (
-      <Image
-        width={16}
-        height={16}
-        src={jeff}
-        alt="a symbol of avarice"
-        className="inline"
-      />
-    );
-  }
+    case itemCount >= 50000000:
+      return <>🧐</>;
 
-  return null;
+    case itemCount >= 100000000:
+      return (
+        <Image
+          width={16}
+          height={16}
+          src={jeff}
+          alt="a symbol of avarice"
+          className="inline"
+        />
+      );
+
+    default:
+      return null;
+  }
 }
